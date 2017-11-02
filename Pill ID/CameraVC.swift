7c26207d89e64a0ca75d2a72d35d20eadaca92b5
Vatsal Rustagi <vatsalr23@gmail.com>
2017-11-02 11:44:48 -0700

    UI Updates and added ARSceneKit

---
+++ Pill ID/CameraVC.swift
@@ -0,0 +1,36 @@
+//
+//  CameraVC.swift
+//  Pill ID
+//
+//  Created by Vatsal Rustagi on 11/2/17.
+//  Copyright © 2017 MedAppJam. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class CameraVC: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let configuration = ARWorldTrackingConfiguration()
+        
+        sceneView.session.run(configuration, options: [])
+        
+        sceneView.automaticallyUpdatesLighting = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        sceneView.session.pause()
+    }
+
+}
