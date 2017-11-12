8e6a1815296a42da2dd47147d9db35cdf0b93717
Vatsal Rustagi <vatsalr23@gmail.com>
2017-11-11 18:49:42 -0800

    Added AR text

--- Pill ID/CameraVC.swift
+++ Pill ID/CameraVC.swift
@@ -12,6 +12,8 @@ import ARKit
 class CameraVC: UIViewController {
 
     @IBOutlet weak var sceneView: ARSCNView!
+    let bubbleDepth : Float = 0.01
+    var latestPrediction : String = "…"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +21,12 @@ class CameraVC: UIViewController {
         let scene = SCNScene(named: "art.scnassets/goku.scn")!
         
         sceneView.scene = scene
+    }
+    
+    @IBAction func moreInfo(_ sender: UIButton) {
+        let node : SCNNode = createNewBubbleParentNode("Advil")
+        sceneView.scene.rootNode.addChildNode(node)
+        node.position = SCNVector3(0,0,0)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,6 +43,47 @@ class CameraVC: UIViewController {
         super.viewWillDisappear(animated)
         
         sceneView.session.pause()
+    }
+    
+    func createNewBubbleParentNode(_ text : String) -> SCNNode {
+        // Warning: Creating 3D Text is susceptible to crashing. To reduce chances of crashing; reduce number of polygons, letters, smoothness, etc.
+        
+        // TEXT BILLBOARD CONSTRAINT
+        let billboardConstraint = SCNBillboardConstraint()
+        billboardConstraint.freeAxes = SCNBillboardAxis.Y
+        
+        // BUBBLE-TEXT
+        let bubble = SCNText(string: text, extrusionDepth: CGFloat(bubbleDepth))
+        var font = UIFont(name: "Avenir-Heavy", size: 0.15)
+//        font = font?.withTraits(traits: .traitBold)
+        bubble.font = font
+        bubble.alignmentMode = kCAAlignmentCenter
+        bubble.firstMaterial?.diffuse.contents = UIColor.orange
+        bubble.firstMaterial?.specular.contents = UIColor.white
+        bubble.firstMaterial?.isDoubleSided = true
+        // bubble.flatness // setting this too low can cause crashes.
+        bubble.chamferRadius = CGFloat(bubbleDepth)
+        
+        // BUBBLE NODE
+        let (minBound, maxBound) = bubble.boundingBox
+        let bubbleNode = SCNNode(geometry: bubble)
+        // Centre Node - to Centre-Bottom point
+        bubbleNode.pivot = SCNMatrix4MakeTranslation( (maxBound.x - minBound.x)/2, minBound.y, bubbleDepth/2)
+        // Reduce default text size
+        bubbleNode.scale = SCNVector3Make(0.2, 0.2, 0.2)
+        
+        // CENTRE POINT NODE
+        let sphere = SCNSphere(radius: 0.005)
+        sphere.firstMaterial?.diffuse.contents = UIColor.cyan
+        let sphereNode = SCNNode(geometry: sphere)
+        
+        // BUBBLE PARENT NODE
+        let bubbleNodeParent = SCNNode()
+        bubbleNodeParent.addChildNode(bubbleNode)
+        bubbleNodeParent.addChildNode(sphereNode)
+        bubbleNodeParent.constraints = [billboardConstraint]
+        
+        return bubbleNodeParent
     }
 
 }
